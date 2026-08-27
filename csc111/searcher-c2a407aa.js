cd581819cf07fded8fb31baf6294594bf5232424
Zach Kohlberg <zach.kohlberg@gmail.com>
2026-08-27 11:09:08 -0400

    fix program name in practice

--- csc111/searcher-c2a407aa.js
+++ csc111/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-04bbffb1.js',
+                path_to_root + 'searchindex-2e23e3ca.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
