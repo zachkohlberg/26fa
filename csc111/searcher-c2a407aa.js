bf8a227f491ed61ee7d8a90a47cd6f6d3e858894
Zach Kohlberg <zach.kohlberg@gmail.com>
2026-09-15 14:08:01 -0400

    w4 material

--- csc111/searcher-c2a407aa.js
+++ csc111/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e8ebd552.js',
+                path_to_root + 'searchindex-53f3dd4e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
